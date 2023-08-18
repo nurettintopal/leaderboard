039fac057f59d1c08e7c2998a639df1c72182427
Nurettin Topal <nurettintopal@gmail.com>
2023-08-18 21:26:57 +0300

    feature: add multiple board feature

---
+++ leaderboard.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"github.com/go-redis/redis/v8"
+)
+
+type Player struct {
+	ID       string
+	Score    int
+	Username string
+}
+
+type LeaderboardService struct {
+	redisClient *redis.Client
+}
+
+func NewLeaderboardService(redisClient *redis.Client) *LeaderboardService {
+	return &LeaderboardService{redisClient: redisClient}
+}
+
+func (ls *LeaderboardService) ClearLeaderboard(leaderboardName string) error {
+	return ls.redisClient.Del(ctx, leaderboardName).Err()
+}
+
+func (ls *LeaderboardService) AddPlayer(leaderboardName string, player Player) error {
+	return ls.redisClient.ZAdd(ctx, leaderboardName, &redis.Z{
+		Score:  float64(player.Score),
+		Member: player.ID,
+	}).Err()
+}
+
+func (ls *LeaderboardService) GetLeaderboard(leaderboardName string) ([]redis.Z, error) {
+	return ls.redisClient.ZRevRangeWithScores(ctx, leaderboardName, 0, -1).Result()
+}
+
+func (ls *LeaderboardService) GetPlayerRank(leaderboardName string, playerID string) (int64, error) {
+	return ls.redisClient.ZRevRank(ctx, leaderboardName, playerID).Result()
+}
--- main.go
+++ main.go
@@ -11,60 +11,30 @@ import (
 
 var ctx = context.Background()
 
-type Player struct {
-	ID       string
-	Score    int
-	Username string
-}
-
-type LeaderboardService struct {
-	redisClient *redis.Client
-}
-
-func NewLeaderboardService(redisClient *redis.Client) *LeaderboardService {
-	return &LeaderboardService{redisClient: redisClient}
-}
-
-func (ls *LeaderboardService) ClearLeaderboard() error {
-	return ls.redisClient.Del(ctx, "leaderboard").Err()
-}
-
-func (ls *LeaderboardService) AddPlayer(player Player) error {
-	return ls.redisClient.ZAdd(ctx, "leaderboard", &redis.Z{
-		Score:  float64(player.Score),
-		Member: player.ID,
-	}).Err()
-}
-
-func (ls *LeaderboardService) GetLeaderboard() ([]redis.Z, error) {
-	return ls.redisClient.ZRevRangeWithScores(ctx, "leaderboard", 0, -1).Result()
-}
-
-func (ls *LeaderboardService) GetPlayerRank(playerID string) (int64, error) {
-	return ls.redisClient.ZRevRank(ctx, "leaderboard", playerID).Result()
-}
-
 func main() {
 	client := createRedisClient()
 	defer client.Close()
 
 	leaderboardService := NewLeaderboardService(client)
 
-	// Clear any previous data
-	if err := leaderboardService.ClearLeaderboard(); err != nil {
-		log.Fatalf("Failed to clear leaderboard: %v", err)
+	var leaderboardName string
+	for i := 0; i < 10; i++ {
+		leaderboardName = fmt.Sprintf("leadershipboard-%d", i)
+
+		// Clear any previous data
+		if err := leaderboardService.ClearLeaderboard(leaderboardName); err != nil {
+			log.Fatalf("Failed to clear leaderboard: %v", err)
+		}
+
+		addSamplePlayers(leaderboardService, leaderboardName, 5)
+
+		displayLeaderboard(leaderboardService, leaderboardName)
+		displayRank(leaderboardService, leaderboardName, "player3")
+
+		fmt.Println("")
+		fmt.Println("--------")
+		fmt.Println("")
 	}
-
-	addSamplePlayers(leaderboardService, 100)
-
-	displayLeaderboard(leaderboardService)
-	displayRank(leaderboardService, "player2")
-	displayRank(leaderboardService, "player4")
-	displayRank(leaderboardService, "player8")
-	displayRank(leaderboardService, "player16")
-	displayRank(leaderboardService, "player32")
-	displayRank(leaderboardService, "player64")
-
 }
 
 func createRedisClient() *redis.Client {
@@ -75,7 +45,7 @@ func createRedisClient() *redis.Client {
 	})
 }
 
-func addSamplePlayers(service *LeaderboardService, count int) {
+func addSamplePlayers(service *LeaderboardService, leaderboardName string, count int) {
 	for i := 1; i <= count; i++ {
 		player := Player{
 			ID:       fmt.Sprintf("player%d", i),
@@ -83,19 +53,20 @@ func addSamplePlayers(service *LeaderboardService, count int) {
 			Username: fmt.Sprintf("User%d", i),
 		}
 
-		if err := service.AddPlayer(player); err != nil {
+		if err := service.AddPlayer(leaderboardName, player); err != nil {
 			log.Printf("Failed to add player %s to leaderboard: %v", player.ID, err)
 		}
 	}
 }
 
-func displayLeaderboard(service *LeaderboardService) {
-	leaderboard, err := service.GetLeaderboard()
+func displayLeaderboard(service *LeaderboardService, leaderboardName string) {
+	leaderboard, err := service.GetLeaderboard(leaderboardName)
 	if err != nil {
 		log.Fatalf("Failed to retrieve leaderboard: %v", err)
 	}
 
-	fmt.Println("Leaderboard:")
+	fmt.Printf("Board for %s:", leaderboardName)
+	fmt.Println()
 	for rank, member := range leaderboard {
 		playerID := member.Member.(string)
 		playerScore := int(member.Score)
@@ -103,8 +74,8 @@ func displayLeaderboard(service *LeaderboardService) {
 	}
 }
 
-func displayRank(service *LeaderboardService, targetPlayerID string) {
-	rank, err := service.GetPlayerRank(targetPlayerID)
+func displayRank(service *LeaderboardService, leaderboardName string, targetPlayerID string) {
+	rank, err := service.GetPlayerRank(leaderboardName, targetPlayerID)
 	if err != nil {
 		log.Fatalf("Failed to retrieve rank for player %s: %v", targetPlayerID, err)
 	}
