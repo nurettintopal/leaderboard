e65674a5e2e9cd216675f19313de51590e19097f
Nurettin Topal <nurettintopal@gmail.com>
2023-08-18 20:45:37 +0300

    refactor: clean code principles

--- main.go
+++ main.go
@@ -18,35 +18,62 @@ type Player struct {
 }
 
 func main() {
-	// Create a Redis client
-	client := redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379", // Redis server address
-		Password: "",               // No password
-		DB:       0,                // Default DB
+	client := createRedisClient()
+	defer client.Close()
+
+	clearLeaderboard(client)
+	addSamplePlayers(client, 100)
+
+	displayLeaderboard(client)
+	displayRank(client, "player2")
+	displayRank(client, "player4")
+	displayRank(client, "player8")
+	displayRank(client, "player16")
+	displayRank(client, "player32")
+	displayRank(client, "player64")
+}
+
+func createRedisClient() *redis.Client {
+	return redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
 	})
+}
 
-	// Clear any previous data
-	client.Del(ctx, "leaderboard")
+func clearLeaderboard(client *redis.Client) {
+	err := client.Del(ctx, "leaderboard").Err()
+	if err != nil {
+		log.Fatalf("Failed to clear leaderboard: %v", err)
+	}
+}
 
-	// Simulate adding players with random scores
-	for i := 1; i <= 100; i++ {
+func addSamplePlayers(client *redis.Client, count int) {
+	for i := 1; i <= count; i++ {
 		player := Player{
 			ID:       fmt.Sprintf("player%d", i),
 			Score:    rand.Intn(1000),
 			Username: fmt.Sprintf("User%d", i),
 		}
 
-		// Add player to the leaderboard
-		client.ZAdd(ctx, "leaderboard", &redis.Z{
-			Score:  float64(player.Score),
-			Member: player.ID,
-		})
+		addPlayerToLeaderboard(client, player)
 	}
+}
 
-	// Retrieve and display the leaderboard
+func addPlayerToLeaderboard(client *redis.Client, player Player) {
+	err := client.ZAdd(ctx, "leaderboard", &redis.Z{
+		Score:  float64(player.Score),
+		Member: player.ID,
+	}).Err()
+	if err != nil {
+		log.Printf("Failed to add player %s to leaderboard: %v", player.ID, err)
+	}
+}
+
+func displayLeaderboard(client *redis.Client) {
 	leaderboard, err := client.ZRevRangeWithScores(ctx, "leaderboard", 0, -1).Result()
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Failed to retrieve leaderboard: %v", err)
 	}
 
 	fmt.Println("Leaderboard:")
@@ -55,12 +82,12 @@ func main() {
 		playerScore := int(member.Score)
 		fmt.Printf("%d. %s - Score: %d\n", rank+1, playerID, playerScore)
 	}
+}
 
-	// Get the rank of a specific player
-	targetPlayerID := "player5"
+func displayRank(client *redis.Client, targetPlayerID string) {
 	rank, err := client.ZRevRank(ctx, "leaderboard", targetPlayerID).Result()
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Failed to retrieve rank for player %s: %v", targetPlayerID, err)
 	}
 
 	fmt.Printf("%s is ranked #%d on the leaderboard.\n", targetPlayerID, rank+1)
